ca33c9f546a17967d9633609c3e6c5ce76b5c939
Gabriel <Gabriel@192.168.0.68>
2021-09-23 17:45:15 -0300

    Palindromo

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -1,5 +1,36 @@
 package view;
 
+import javax.swing.JOptionPane;
+
+import br.edu.fateczl.GabrielGuerra.pilhaString;
+import controller.ControllerPalindromo;
+
 public class Principal {
+
+	public static void main(String[] args) {
+		Pilha p = new Pilha();
+		ControllerPalindromo pC = new ControllerPalindromo();
+		
+		String inverte = null;
+		boolean validaStr;
+		
+		String entrada = JOptionPane.showInputDialog("Digite uma palavra");
+
+		try {
+			inverte = pC.invertePalavra(p, entrada);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		validaStr = pC.comparaPalavras(entrada, inverte);
+		
+		if(validaStr) {
+//			System.out.println("A palavra " +entrada+ " é igual a "+inverte+ " então é um palíndromo");
+			JOptionPane.showMessageDialog(null, "A palavra " +entrada+ " é igual a "+inverte+ " então é um palíndromo");
+		}else {
+//			System.out.println("A palavra " +entrada+ " não é igual a "+inverte+ " então não é um palíndromo");
+			JOptionPane.showMessageDialog(null, "A palavra " +entrada+ " não é igual a "+inverte+ " então não é um palíndromo");
+		}
+	}
 
 }
